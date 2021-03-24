9f7d4866b2350819203e5f089ba1b7c44f5ae8ea
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2021-03-24 19:09:15 +0100

    chore: Send acquired targets to pulsar
    - Sends acquired targets to pulsar
    - Move gRPC to it's on go routine

--- destroyer/main.go
+++ destroyer/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"time"
 
 	pb "roava-test/pb"
 
 	// "github.com/jackc/pgx/v4"
+	"github.com/apache/pulsar-client-go/pulsar"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	port := ":5000"
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
+	port := 5000
+	psUrl := "pulsar://pulsar:6650"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	// _, e := pgx.Connect(ctx, "") //TODO: Setup PGSQL connection
 
@@ -22,17 +30,48 @@ func main() {
 	// 	log.Fatalf("Database initialization error %v", e.Error())
 	// }
 
-	listen, e := net.Listen("tcp", port)
+	// Create pub-sub client, using pulsar connection
+	pubSub, e := pulsar.NewClient(pulsar.ClientOptions{
+		URL:              psUrl,
+		OperationTimeout: 5 * time.Second,
+	})
+
+	if e != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", e)
+	}
+
+	defer pubSub.Close()
+
+	listen, e := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if e != nil {
 		log.Fatalf("Failed to listen . %v", e.Error())
 	}
 
 	s := grpc.NewServer()
 
-	pb.RegisterDestroyerServer(s, &destroyer{})
+	pb.RegisterDestroyerServer(s, &destroyer{
+		Ctx:          ctx,
+		PubSubClient: pubSub,
+	})
 	reflection.Register(s)
 
-	if e := s.Serve(listen); e != nil {
-		log.Fatalf("Failed to serve gRPC %v", e.Error())
-	}
+	// gRCP blocks main routine so I move it to it's on routine
+	go func() {
+		if e := s.Serve(listen); e != nil {
+			log.Fatalf("Failed to serve gRPC %v", e.Error())
+		}
+	}()
+
+	//
+	log.Printf("Destroyer is running on port %d\n", port)
+
+	/*
+		Prevent destroyer from shutting down when main routine is done
+		because gRPC is listening on another routine
+	*/
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	<-sigint
+	log.Println("Destroyer shutting down")
 }
--- destroyer/server.go
+++ destroyer/server.go
@@ -8,27 +8,60 @@ import (
 
 	pb "roava-test/pb"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // destroyer - implements all destroyer methods
-type destroyer struct{}
+type destroyer struct {
+	Ctx          context.Context
+	PubSubClient pulsar.Client
+}
 
 // AcquireTargets - implements AcquireTargets
-func (s *destroyer) AcquireTargets(ctx context.Context, req *pb.AcquireTargetRequest) (*empty.Empty, error) {
-	targets := []pb.Target{}
+func (s *destroyer) AcquireTargets(ctx context.Context, req *pb.AcquireTargetsRequest) (*empty.Empty, error) {
+	targets := []*pb.Target{}
 
 	for i := int64(0); i < req.GetNumber(); i++ {
-		id := uuid.New()
+		// Create reusable Target properties
 		t := time.Now().UTC().Format(time.RFC3339)
+		id := uuid.New().String()
 
-		targets = append(targets, pb.Target{
-			Id:        id.String(),
+		// Create and append Target to slice
+		targets = append(targets, &pb.Target{
+			Id:        id,
 			Message:   fmt.Sprintf("Unique target %v", id),
 			CreatedOn: t,
 			UpdatedOn: t,
 		})
+
+		// Create Pulsar producer
+		producer, err := s.PubSubClient.CreateProducer(pulsar.ProducerOptions{
+			Topic: "targets-acquired-event",
+		})
+
+		// Build message payload
+		payload := pb.TargetsAcquiredPayload{
+			Id:        uuid.New().String(),
+			Name:      "targets.acquired",
+			Data:      targets,
+			CreatedOn: time.Now().UTC().Format(time.RFC3339),
+		}
+
+		// Send pub-sub message
+		producer.Send(s.Ctx, &pulsar.ProducerMessage{
+			Payload: []byte(payload.String()),
+		})
+
+		// Handles errors
+		if err != nil {
+			return &emptypb.Empty{}, err
+		}
+
+		// Close producer
+		defer producer.Close()
 	}
 
 	log.Printf("%d target(s) acquired\n", req.GetNumber())
